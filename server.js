49fef98e78dd80f1364b3090fb1ad66ca11f4e1f
Vanessa Malloy <catwoman.fan.84@gmail.com>
2021-01-12 18:56:05 -0800

    set up server file

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 var express = require("express");
+var exphbs = require("express-handlebars");
+var routes = require("./controllers/burgers_controllers.js");
+
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server is listening on: http://localhost:" + PORT);
+});
