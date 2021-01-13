d8ed30c93c31e3a75ce43960f9f41614f8323e3e
Vanessa Malloy <catwoman.fan.84@gmail.com>
2021-01-12 20:19:14 -0800

    starting index.handlebars layout

--- models/burger.js
+++ models/burger.js
@@ -3,4 +3,8 @@ var orm = require ("../config/orm");
 
 // Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
 
+var burger = {
+    
+}
+
 module.exports = burger;
