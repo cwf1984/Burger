143f959d3149f1b36defa3a747c5e78fca5673ac
Vanessa Malloy <catwoman.fan.84@gmail.com>
2021-01-12 18:45:29 -0800

    folder and file setup

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name)
+VALUES ("Mushroom Swiss Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Black and Bleu Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Pineapple Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Bacon Cheeseburger");
---
+++ models/burgers_controllers.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var burgers = require("../models/burgers");
+
+var router = express.Router();
+
+router.get("/", function(req, res) {
+
+});
+
+router.post("/api/burgers", function(req, res) {
+
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+
+});
+
+router.delete("api/burgers/:id", function(req, res) {
+
+});
+
+module.exports = router;
